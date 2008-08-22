23dff85cd78d742d45d75f7b523c9e31de7ea3cf
Maverick Edwards <medwards@nasba.org>
2008-08-22 17:43:19 +0000

    git-svn-id: http://svn.nasba.dev/Voodoo/core@7740 cf590dee-7fe1-0310-af51-ab8780c2a794

--- Voodoo.pm
+++ Voodoo.pm
@@ -10,7 +10,7 @@
 ####################################################################################
 package Apache::Voodoo;
 
-$VERSION = ('$HeadURL$' =~ m!([^/]+)(?:/[^/]+)\$!)[0];
+$VERSION = sprintf("%0.4f",('$HeadURL$' =~ m!(\d+\.\d+)!)[0]);
 
 use strict;
 use Data::Dumper;
