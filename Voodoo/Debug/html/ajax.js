5e5483ab435a6d64a4d92c4004b45e67db90594f
Maverick Edwards <medwards@nasba.org>
2008-09-25 21:53:54 +0000

    git-svn-id: http://svn.nasba.dev/Voodoo/core@8286 cf590dee-7fe1-0310-af51-ab8780c2a794

--- Voodoo/Debug/html/ajax.js
+++ Voodoo/Debug/html/ajax.js
@@ -26,48 +26,54 @@ function voodooDebug(opts){
 
 	this.spinner = new Image(16,16);
 	this.spinner.src = this.debug_root+"/spinner.gif";
+	this.minus = new Image(16,16);
+	this.minus.src = this.debug_root+"/minus.gif";
+	this.plus = new Image(16,16);
+	this.plus.src = this.debug_root+"/plus.gif";
+
 	this.yourBrowserIsBroken=(navigator.userAgent.toLowerCase().indexOf("msie")!=-1);
 
 	this.createRequestObject=function(){try {var ro=new XMLHttpRequest();}catch(e){var ro=new ActiveXObject("Microsoft.XMLHTTP");}return ro;};this.sndReq=function(action,url,data){if(action.toUpperCase()=="POST"){this.http.open(action,url,true);this.http.setRequestHeader('Content-Type','application/x-www-form-urlencoded');this.http.onreadystatechange=this.handleResponse;this.http.send(data);}else{this.http.open(action,url+'?'+data,true);this.http.onreadystatechange=this.handleResponse;this.http.send(null);}};this.handleResponse=function(){if(me.http.readyState==4){if(typeof me.funcDone=='function'){me.funcDone();}var rawdata=me.http.responseText;
 
 	var data = me.parse(rawdata);
-
-	console.log(data);
 	if (data.value != null && data.value.length) {
-		if (data.value.constructor == Array) {
-			var h = '<dl>';
-			for (j=0; j < data.value.length; j++) {
-				console.log(j);
-				h += '<dt class="vdClosed" onClick="vdDebug.toggleDL(this);">'+
-					'<img src="'+me.debug_root+'/plus.gif" />'+
-					data.value[j][0].replace(/>/g,'&gt;')
-					+'</dt><dd class="vdClosed">'+
-					data.value[j][1].replace(/</g,'&lt;')+
-					'</dd>';
-			}
-			h += '</dl>';
-			document.getElementById(data.key).innerHTML = h;
-		}
-		else {
-			document.getElementById(data.key).innerHTML = data.value;
-		}
+		document.getElementById(data.key).innerHTML = me.loadDisplay(data.value);
 	}
 	else {
 		document.getElementById(data.key).innerHTML = "<i>(empty)</i>";
 	}
 
 }if ((me.http.readyState==1)&&(typeof me.funcWait=='function')){me.funcWait();}};var me=this;this.http=this.createRequestObject();var funcWait=null;var funcDone=null;this.f=function(n){return n<10?'0'+n:n;};if(typeof Date.prototype.toJSON!=='function'){Date.prototype.toJSON=function(key){return this.getUTCFullYear()+'-'+f(this.getUTCMonth()+1)+'-'+f(this.getUTCDate())+'T'+f(this.getUTCHours())+':'+f(this.getUTCMinutes())+':'+f(this.getUTCSeconds())+'Z';};String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(key){return this.valueOf();};}var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapeable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;this.parse=function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v;}else{delete value[k];}}}}return reviver.call(holder,key,value);}cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return '\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4);});}if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j;}throw new SyntaxError('JSON.parse');}
+	this.loadDisplay = function(data) {
+		var h;
+		if (data.constructor == Array) {
+			h = '<dl>';
+			for (j=0; j < data.length; j++) {
+				console.log(j);
+				h += '<dt class="vdClosed" onClick="vdDebug.toggleDL(this);">'+
+					'<img src="'+this.debug_root+'/plus.gif" />'+
+					data[j][0].replace(/>/g,'&gt;')
+					+'</dt><dd class="vdClosed">'+
+					data[j][1].replace(/</g,'&lt;')+
+					'</dd>';
+			}
+			h += '</dl>';
+		}
+		else {
+			h = "<span>"+data.value+"</span>";
+		}
+		return h;
+	}
 
 	this.toggleDL = function(obj) {
-		console.log(obj.nextSibling);
 		if (obj.className == "vdOpen") {
 			obj.className = "vdClosed";
-			obj.firstChild.src=this.debug_root+"/plus.gif";
+			obj.firstChild.src=this.plus.src;
 			obj.nextSibling.className = "vdClosed";
 		}
 		else {
 			obj.className = "vdOpen";
-			obj.firstChild.src=this.debug_root+"/minus.gif";
+			obj.firstChild.src=this.minus.src;
 			obj.nextSibling.className = "vdOpen";
 		}
 	}
@@ -75,11 +81,11 @@ function voodooDebug(opts){
 	this.handleSection = function(obj, section) {
 		if (obj.parentNode.className == "vdOpen") {
 			obj.parentNode.className = 'vdClosed';
-			obj.firstChild.src=this.debug_root+"/plus.gif";
+			obj.firstChild.src=this.plus.src;
 		}
 		else {
 			obj.parentNode.className = 'vdOpen';
-			obj.firstChild.src=this.debug_root+"/minus.gif";
+			obj.firstChild.src=this.minus.src;
 
 			if (section != "top") {
 				document.getElementById("vd_"+section).innerHTML = '<img src="'+this.spinner.src+'">';	
