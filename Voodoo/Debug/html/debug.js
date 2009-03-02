e709b97b69b545f6a97bec3e840b39a0a953ec79
Maverick Edwards <medwards@nasba.org>
2009-03-02 08:22:55 +0000

    git-svn-id: http://svn.nasba.dev/Voodoo/core@13002 cf590dee-7fe1-0310-af51-ab8780c2a794

--- Voodoo/Debug/html/debug.js
+++ Voodoo/Debug/html/debug.js
@@ -1,24 +1,35 @@
 // $Id$
 //
-// Ajax handler based on Sean Kane's Feather Ajax.  It has been modified to parse JSON responses using the reference implementation
-// found at JSON.org.  Original copy right notices appear below.
-//
+// Ajax handler for Voodoo's native debugging functions.  This object contains  
+// both Sean Kane's Feather Ajax and the reference JSON parser found at JSON.org.
+// The original copyright notices for those components appear below, along with
+// comments in the code noting where they begin, end and how they were modified.
 
 //Created by Sean Kane (http://celtickane.com/programming/code/ajax.php)
 //Feather Ajax v1.0.1
 
 /*
     http://www.JSON.org/json2.js
-    2008-09-01
+    2008-11-19
 
     Public Domain.
 
     NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 
     See http://www.JSON.org/js.html
+
+	This is a reference implementation. You are free to copy, modify, or
+    redistribute.
+
+    This code should be minified before deployment.
+    See http://javascript.crockford.com/jsmin.html
+
+    USE YOUR OWN COPY. IT IS EXTREMELY UNWISE TO LOAD CODE FROM SERVERS YOU DO
+    NOT CONTROL.
+
 */
 
-function voodooDebug(opts){
+function voodooDebug(opts) {
 	this.debug_root = opts.debug_root;
 	this.app_id     = opts.app_id;
 	this.session_id = opts.session_id;
@@ -39,68 +50,129 @@ function voodooDebug(opts){
 		this.imgLevels[levels[i]].src = this.debug_root+"/i/"+levels[i]+".png";
 	}
 
-	this.yourBrowserIsBroken=(navigator.userAgent.toLowerCase().indexOf("msie")!=-1);
+	this.yourBrowserIsBroken = (navigator.userAgent.toLowerCase().indexOf("msie")!=-1);
 
-	this.createRequestObject=function(){try {var ro=new XMLHttpRequest();}catch(e){var ro=new ActiveXObject("Microsoft.XMLHTTP");}return ro;};this.sndReq=function(action,url,data){if(action.toUpperCase()=="POST"){this.http.open(action,url,true);this.http.setRequestHeader('Content-Type','application/x-www-form-urlencoded');this.http.onreadystatechange=this.handleResponse;this.http.send(data);}else{this.http.open(action,url+'?'+data,true);this.http.onreadystatechange=this.handleResponse;this.http.send(null);}};this.handleResponse=function(){if(me.http.readyState==4){if(typeof me.funcDone=='function'){me.funcDone();}var rawdata=me.http.responseText;
+	//////////////////////////////////////////////////////////////////////////////////
+	// Beginning of Feather Ajax
+	//////////////////////////////////////////////////////////////////////////////////
 
-	console.log(eval('('+rawdata+')'));
-	var data = me.parse(rawdata);
-	if (data.value != null && data.value.length) {
-		document.getElementById(data.key).innerHTML = me.loadDisplay(data.value);
+	this.createRequestObject = function() {
+		try {
+			var ro=new XMLHttpRequest();
+		}
+		catch(e) {
+			var ro=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		return ro;
 	}
-	else {
-		document.getElementById(data.key).innerHTML = "<i>(empty)</i>";
-	}
 
-}if ((me.http.readyState==1)&&(typeof me.funcWait=='function')){me.funcWait();}};var me=this;this.http=this.createRequestObject();var funcWait=null;var funcDone=null;this.f=function(n){return n<10?'0'+n:n;};if(typeof Date.prototype.toJSON!=='function'){Date.prototype.toJSON=function(key){return this.getUTCFullYear()+'-'+f(this.getUTCMonth()+1)+'-'+f(this.getUTCDate())+'T'+f(this.getUTCHours())+':'+f(this.getUTCMinutes())+':'+f(this.getUTCSeconds())+'Z';};String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(key){return this.valueOf();};}var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapeable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;
-	this.parse=function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v;}else{delete value[k];}}}}return reviver.call(holder,key,value);}cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return '\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4);});}if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j;}throw new SyntaxError('JSON.parse');}
-	this.loadDisplay = function(data) {
-		var h;
-		if (data.constructor == Array) {
-			if (data[0][1].constructor == Array) {
-				h = '<ul>';
-				for (j=0; j < data.length; j++) {
-					h += '<li class="vdOpen"><span onClick="vdDebug.toggleUL(this);">'+
-						 '<img src="'+this.imgMinus.src+'" />'+
-					     data[j][0]+'</span>'+
-					     this.loadDisplay(data[j][1])+
-						 '</li>';
-				}
-				h += '</ul>';
-			}
-			else if (data[0].length == 2) {
-				h = '<dl>';
-				for (j=0; j < data.length; j++) {
-					console.log(j);
-					h += '<dt class="vdClosed" onClick="vdDebug.toggleDL(this);">'+
-						 '<img src="'+this.imgPlus.src+'" />'+
-						 data[j][0].replace(/>/g,'&gt;')
-						 +'</dt><dd class="vdClosed">'+
-						 data[j][1].replace(/</g,'&lt;')+
-						 '</dd>';
-				}
-				h += '</dl>';
-			}
-			else if (data[0].constructor == Array) {
-				h = '<table><tr><th>';
-				h += data[0].join('</th><th>');
-				h += '</th></tr>';
-
-				for (j=1; j < data.length; j++) {
-					h += '<tr><td><pre>';
-					h += data[j].join('</pre></td><td><pre>');
-					h += '</td></tr>';
-				}
-				h += "</table>";
-			}
-			else {
-				h = "<pre>"+data+"</pre>";
-			}
+	this.http = this.createRequestObject();
+	var me = this;
+	this.sndReq = function(action,url,data) {
+		if (action.toUpperCase()=="POST") {
+			this.http.open(action,url,true);
+			this.http.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+			this.http.onreadystatechange=this.handleResponse;
+			this.http.send(data);
 		}
 		else {
-			h = "<span>"+data+"</span>";
+			this.http.open(action,url+'?'+data,true);
+			this.http.onreadystatechange=this.handleResponse;
+			this.http.send(null);
 		}
+	}
+
+	this.handleResponse = function() {
+		// Callbacks stripped;
+		if (me.http.readyState==4) {
+			me.handleDisplay(me.http.responseText);
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////
+	// End of Feather Ajax
+	//////////////////////////////////////////////////////////////////////////////////
+
+	this.handleProfile = function(data) {
+		var h = '<table><tr><th>';
+		h += data[0].join('</th><th>');
+		h += '</th></tr>';
+
+		for (j=1; j < data.length; j++) {
+			h += '<tr><td><pre>';
+			h += data[j].join('</pre></td><td><pre>');
+			h += '</td></tr>';
+		}
+		h += "</table>";
 		return h;
+	}
+
+	this.handleDebug = function(data) {
+		var h = '<dl>';
+		for (j=0; j < data.length; j++) {
+			h += '<dt class="vdClosed" onClick="vdDebug.toggleDL(this);">'+
+				'<img src="'+this.imgPlus.src+'" />'+
+				data[j][0].replace(/>/g,'&gt;')
+				+'</dt><dd class="vdClosed">'+
+				data[j][1].replace(/</g,'&lt;')+
+				'</dd>';
+		}
+		h += '</dl>';
+		return h;
+	}
+
+	this.handleReturnData = function(data) {
+		var h = '<dl>';
+		for (j=0; j < data.length; j++) {
+			h += '<dt class="vdClosed" onClick="vdDebug.toggleDL(this);">'+
+				'<img src="'+this.imgPlus.src+'" />'+
+				data[j][0].replace(/>/g,'&gt;')
+				+'</dt><dd class="vdClosed">'+
+				data[j][1].replace(/</g,'&lt;')+
+				'</dd>';
+		}
+		h += '</dl>';
+		return h;
+
+	}
+
+	this.handleData = function(data) {
+		var h = '<ul>';
+		for (j=0; j < data.length; j++) {
+			h += '<li class="vdOpen"><span onClick="vdDebug.toggleUL(this);">'+
+				'<img src="'+this.imgMinus.src+'" />'+
+				data[j][0]+'</span>'+
+				this.handleData(data[j][1])+
+				'</li>';
+		}
+		h += '</ul>';
+		return h;
+	}
+
+	this.handleDisplay = function(rawdata) {
+		var data = this.parse(rawdata);
+		console.log(data);
+
+		var h;
+		if (data.value == null || data.value.length <= 0) {
+			h = "<i>(empty)</i>";
+		}
+		else {
+			if (data.constructor == Object) {
+				switch (data.key) {
+					case 'vd_profile':     h = this.handleProfile(   data.value); break;
+					case 'vd_debug':       h = this.handleDebug(     data.value); break;
+					case 'vd_return_data': h = this.handleReturnData(data.value); break;
+					default:               h = this.handleData(      data.value); break;
+				}
+			}
+			else {
+				h = "<span>"+data+"</span>";
+			}
+		}
+		console.log(h);
+
+		document.getElementById(data.key).innerHTML = h;
 	}
 
 	this.toggleDL = function(obj) {
@@ -120,12 +192,10 @@ function voodooDebug(opts){
 		if (obj.parentNode.className == "vdOpen") {
 			obj.parentNode.className = "vdClosed";
 			obj.firstChild.src=this.imgPlus.src;
-			//obj.nextSibling.className = "vdClosed";
 		}
 		else {
 			obj.parentNode.className = "vdOpen";
 			obj.firstChild.src=this.imgMinus.src;
-			//obj.nextSibling.className = "vdOpen";
 		}
 	}
 
@@ -141,7 +211,7 @@ function voodooDebug(opts){
 			if (section != "top") {
 				document.getElementById("vd_"+section).innerHTML = '<img src="'+this.imgSpinner.src+'">';	
 				this.sndReq('get',this.debug_root+"/"+section,
-					'app_id='+this.app_id+
+					'app_id='     +this.app_id+
 					'&session_id='+this.session_id+
 					'&request_id='+this.request_id
 				);
@@ -166,4 +236,62 @@ function voodooDebug(opts){
 
 	this.toggleFilter = function(obj,section) {
 	}
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////
+	// Start of JSON library.
+	// The stringify function and it's supporting functions have been removed
+	// since this object only needs the parse function.
+	// The comments were extremly verbose and have been removed.
+	//////////////////////////////////////////////////////////////////////////////////
+	this.f = function (n) {
+		return n<10?'0'+n:n;
+	}
+	if (typeof Date.prototype.toJSON !== 'function') {
+		Date.prototype.toJSON = function(key) {
+			return this.getUTCFullYear()+'-'+f(this.getUTCMonth()+1)+'-'+f(this.getUTCDate())+'T'+f(this.getUTCHours())+':'+f(this.getUTCMinutes())+':'+f(this.getUTCSeconds())+'Z';
+		}
+		String.prototype.toJSON = Number.prototype.toJSON = Boolean.prototype.toJSON = function (key) {
+			return this.valueOf();
+		}
+	}
+	var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapeable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;
+
+	this.parse = function(text,reviver) {
+		var j;
+		function walk(holder,key) {
+			var k,v,value = holder[key];
+			if (value&&typeof value==='object') {
+				for (k in value) {
+					if (Object.hasOwnProperty.call(value,k)) {
+						v = walk(value,k);
+						if (v!==undefined) {
+							value[k]=v;
+						}
+						else {
+							delete value[k];
+						}
+					}
+				}
+			}
+			return reviver.call(holder,key,value);
+		}
+		cx.lastIndex=0;
+		if (cx.test(text)) {
+			text=text.replace(cx,
+				function(a) {
+					return '\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4);
+				}
+			);
+		}
+		if (/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))) {
+			j = eval('('+text+')');
+			return typeof reviver==='function'?walk({'':j},''):j;
+		}
+		throw new SyntaxError('voodooDebug.parse');
+	}
+	//////////////////////////////////////////////////////////////////////////////////
+	// End of JSON library
+	//////////////////////////////////////////////////////////////////////////////////
 }
