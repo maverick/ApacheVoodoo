b99c12ece79df82ed307f89e7c21fb08c94a12d7
Maverick Edwards <medwards@nasba.org>
2009-05-18 22:23:53 +0000

    git-svn-id: http://svn.nasba.dev/Voodoo/trunk@15879 cf590dee-7fe1-0310-af51-ab8780c2a794

--- Voodoo/experimental/Soap.pm
+++ Voodoo/experimental/Soap.pm
@@ -7,31 +7,54 @@ use SOAP::Transport::HTTP;
 
 use Data::Dumper;
 
-my $handler = Apache::Voodoo::Soap::Handler->new();
+use constant MP2 => ( exists $ENV{MOD_PERL_API_VERSION} and $ENV{MOD_PERL_API_VERSION} >= 2 ); 
+			  
+=pod
+# setup primary hook to mod_perl depending on which version we're running
+BEGIN {
+	if (MP2) {
+		*handler = sub : method { shift()->handle_request(@_) };
+	}
+	else {
+		*handler = sub ($$) { shift()->handle_request(@_) };
+	}
+}
+=cut
 
-my $server = SOAP::Transport::HTTP::Apache->on_dispatch(\&dispatcher);
-$server->dispatch_to($handler,"target");
+sub new {
+	my $class = shift;
 
+	my $self = {};
+	bless $self,$class;
+
+
+	$self->{handler} = Apache::Voodoo::Soap::Handler->new();
+
+	$self->{soap} = SOAP::Transport::HTTP::Apache->on_dispatch(
+		sub {
+			my $req = shift;
+
+			print STDERR "in on_dispatch closure\n";
+			$self->{handler}->{method} = $req->dataof->name;
+
+			return ("Apache/Voodoo/Soap/Handler","target");
+		}
+	);
+
+	$self->{soap}->dispatch_to($self->{handler},"target");
+
+	return $self;
+}
 
 sub handler { 
-	my $r = $_[0];
+	my ($self,$r) = @_;
 
-	$handler->{uri} = $r->uri();
-	return $server->handler(@_);
+	print STDERR "$$ called ".$self."\n";
+
+	$self->{handler}->{uri} = $r->uri();
+
+	return $self->{soap}->handler($r);
 }
-
-sub dispatcher {
-	my $req = shift;
-
-	open(DUMP,">>/tmp/soap.dump");
-
-	$handler->{method} = $req->dataof->name;
-
-	close(DUMP);
-
-	return ("Apache/Voodoo/Soap/Handler","target");
-}
-
 
 1;
 
