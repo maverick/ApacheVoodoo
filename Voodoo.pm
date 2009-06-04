b7dc5ac863d6790ec2869c24e5209001c5b78d37
Maverick Edwards <medwards@nasba.org>
2009-06-04 19:56:34 +0000

    Made exception() always die regardless of if debugging is turned on.
    git-svn-id: http://svn.nasba.dev/Voodoo/trunk@16248 cf590dee-7fe1-0310-af51-ab8780c2a794

--- Voodoo.pm
+++ Voodoo.pm
@@ -70,8 +70,8 @@ sub exception {
 
 	if (ref($Apache::Voodoo::Engine::debug)) {
 		$Apache::Voodoo::Engine::debug->exception(@_);
-		Apache::Voodoo::Exception::RunTime::Thrown->throw(message => join("\n",@_));
 	}
+	Apache::Voodoo::Exception::RunTime::Thrown->throw(message => join("\n",@_));
 }
 
 sub trace { 
