323e519e597285267ae06607450ab692bb504792
Maverick Edwards <medwards@nasba.org>
2010-08-21 14:55:05 -0500

    handles headings, verbatim, code, < and >

--- wiki/convert.pl
+++ wiki/convert.pl
@@ -11,12 +11,34 @@ foreach my $ifile (@ARGV) {
 
 	my $verbatim = 0;
 	while (my $line=<IN>) {
-		$line =~ s/^---++++++\s*\!*\s*/=head5/;
-		$line =~ s/^---+++++\s*\!*\s*/=head4/;
-		$line =~ s/^---++++\s*\!*\s*/=head3/;
-		$line =~ s/^---+++\s*\!*\s*/=head2/;
-		$line =~ s/^---++\s*\!*\s*/=head1/;
-		$line =~ s/^---+\s*\!*\s*/=head1/;
+		$line =~ s/^---\+\+\+\+\+\+\s*(.*)/=head5 $1\n/;
+		$line =~ s/^---\+\+\+\+\+\s*(.*)/=head4 $1\n/;
+		$line =~ s/^---\+\+\+\+\s*(.*)/=head3 $1\n/;
+		$line =~ s/^---\+\+\+\s*(.*)/=head2 $1\n/;
+		$line =~ s/^---\+\+\s*(.*)/=head1 $1\n/;
+		$line =~ s/^---\+\s*(.*)/=head1 $1\n/;
+
+		$line =~ s/=([^=]+)=/C<$1>/g;
+		
+		$line =~ s/&lt;/E<lt>/g;
+		$line =~ s/&gt;/E<gt>/g;
+		$line =~ s/!!//g;
+		$line =~ s/<nop>//g;
+
+		next if $line =~ /^%META/ or $line =~ /\%TOC/;
+
+		if ($line =~ /<verbatim>/) {
+			$verbatim = 1;
+			print OUT "\n";
+		}
+		elsif ($line =~ /<\/verbatim>/) {
+			$verbatim = 0;
+			print OUT "\n";
+		}
+		else {
+			print OUT "\t" if $verbatim;
+			print OUT $line;
+		}
 	}
 
 	close(IN);
