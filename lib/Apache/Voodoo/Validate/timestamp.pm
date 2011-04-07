926453f64ef9b8b0bf42cdbbd017dfd38afa2a76
Maverick <medwards@nasba.org>
2011-04-07 08:31:16 -0500

    *sigh* hate it when I miss one

---
+++ lib/Apache/Voodoo/Validate/timestamp.pm
@@ -0,0 +1,18 @@
+package Apache::Voodoo::Validate::timestamp;
+
+use strict;
+use warnings;
+
+use base("Apache::Voodoo::Validate::datetime");
+
+1;
+
+################################################################################
+# Copyright (c) 2005-2010 Steven Edwards (maverick@smurfbane.org).
+# All rights reserved.
+#
+# You may use and distribute Apache::Voodoo under the terms described in the
+# LICENSE file include in this package. The summary is it's a legalese version
+# of the Artistic License :)
+#
+################################################################################
